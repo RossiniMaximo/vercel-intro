b7cba51226d947d2a92c4c79760eb5aa86d9748e
Maximo <maximorossini2016@gmail.com>
2022-04-27 12:41:01 -0300

    next

---
+++ pages/api/auth/index.ts
@@ -0,0 +1,8 @@
+import methods from "micro-method-router";
+import { NextApiRequest, NextApiResponse } from "next";
+
+export default methods({
+  async post(req: NextApiRequest, res: NextApiResponse) {
+    res.send("I'm the auth's post method." + " " + req);
+  },
+});
